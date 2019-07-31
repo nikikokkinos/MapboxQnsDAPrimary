5bac4985808093ddb140ee712c05afb0c742e559
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-07-31 15:57:38 -0400

    legend divs show & hide based on which radio button option is selected

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,8 @@ var map = new mapboxgl.Map({
   center: [-73.832966,40.694523],
 })
 
+$('#legendCandidate').hide();
+
 // adding zoom and panning control
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
@@ -93,33 +95,59 @@ map.on('load', function() {
     },
   });
 
+  // // creating variables to hold quantities & colors of Total layer
+  // var layers = ['0', '0-30', '30-50', '50-70', '70-100', '100-125', '125+'];
+  // var colors = ['#FFFFFF', '#fee5d9', '#fcbba1', '#fc9272', '#fb6a4a', '#de2d26', '#a50f15'];
+  //
+  // // creating variables to hold quantities & colors of candidate layers
+  // var candidateLayers = ['0', '0-25', '25-50', '50-75', '75-90', '90-95', '95 +'];
+  // var candidateColors = ['#FFFFFF', '#fee5d9', '#fcbba1', '#fb6a4a', '#de2d26', '#a50f15'];
+
+  // // creating a loop function to create legend with layers and colors on Total layer
+  // for (i = 0; i < layers.length; i++) {
+  //
+  //   var layer = layers[i];
+  //   var color = colors[i];
+  //
+  //   var item = document.createElement('div');
+  //   var key = document.createElement('span');
+  //   key.className = 'legend-key';
+  //   key.style.backgroundColor = color;
+  //
+  //   var value = document.createElement('span');
+  //   value.innerHTML = layer;
+  //   item.appendChild(key);
+  //   item.appendChild(value);
+  //   legend.appendChild(item);
+  // }
+  //
+  // for (i = 0; i < candidateLayers.length; i++) {
+  //
+  //   var candidateLayer = candidateLayers[i];
+  //   var candidateColor = candidateColors[i];
+  //
+  //   var item2 = document.createElement('div');
+  //   var key2 = document.createElement('span');
+  //   key2.className = 'legend-keyCandidate';
+  //   key2.style.backgroundColor = candidateColor;
+  //
+  //   var value2 = document.createElement('span');
+  //   value2.innerHTML = candidateLayer;
+  //   item2.appendChild(key2);
+  //   item2.appendChild(value2);
+  //   legendCandidate.appendChild(item2);
+  // }
+
   map.on('mousemove', function () {
     if (document.getElementById('total').checked) {
-      // creating variables to hold quantities & colors of Total layer
-      var totalLayers = ['0', '0-30', '30-50', '50-70', '70-100', '100-125', '125+'];
-      var totalColors = ['#FFFFFF', '#fee5d9', '#fcbba1', '#fc9272', '#fb6a4a', '#de2d26', '#a50f15'];
 
-      // creating a loop function to run through the Total layer
-      for (i = 0; i < totalLayers.length; i++) {
-        var totalLayer = totalLayers[i];
-        var totalColor = totalColors[i];
-        var item = document.createElement('div');
-        var key = document.createElement('span');
-        key.className = 'legend-key';
-        key.style.backgroundColor = totalColor;
-
-        var value = document.createElement('span');
-        value.innerHTML = totalLayer;
-        item.appendChild(key);
-        item.appendChild(value);
-        legend.appendChild(item);
-      }
+      $('#legend').show();
+      $('#legendCandidate').hide();
 
       map.on('mousemove', function totalFunction(e) {
         var EDs = map.queryRenderedFeatures(e.point, {
           layers: ['Total']
         });
-
         if (EDs.length > 0) {
           document.getElementById('pd').innerHTML = '<h3><strong>' + EDs[0].properties.ElectDist + '</strong></h3><p><strong><em>' + EDs[0].properties.Refactored_Total_Votes_ED + '</strong> votes cast</em></p>';
         } else {
@@ -129,29 +157,14 @@ map.on('load', function() {
   }
 
     if (document.getElementById('tiffany').checked) {
-    //   var candidateLayers = ['0', '0-25', '25-50', '50-75', '75-90', '90-95', '95 +'];
-    //   var candidateColors = ['#FFFFFF', '#fee5d9', '#fcbba1', '#fb6a4a', '#de2d26', '#a50f15'];
-    //   //
-    //   for (i = 0; i < candidateLayers.length; i++) {
-    //     var candidateLayer = candidateLayers[i];
-    //     var candidateColor = candidateColors[i];
-    //     var item = document.createElement('div');
-    //     var key = document.createElement('span');
-    //     key.className = 'legend-keyCandidate';
-    //     key.style.backgroundColor = candidateColor;
-    // }
-    //
-    //   var value = document.createElement('span');
-    //   value.innerHTML = candidateLayer;
-    //   item.appendChild(key);
-    //   item.appendChild(value);
-    //   legendCandidate.appendChild(item);
+
+      $('#legendCandidate').show();
+      $('#legend').hide();
 
       map.on('mousemove', function candidateFunction(e) {
         var cabanEDs = map.queryRenderedFeatures(e.point, {
           layers: ['Caban']
         });
-
         if (cabanEDs.length > 0) {
           document.getElementById('pd').innerHTML = '<h3><strong>' + cabanEDs[0].properties.ElectDist + '</strong></h3><p><strong><em>' + cabanEDs[0].properties.Refactored_Caban_VotePrcnt + '</strong>% of votes cast</em></p>';
         } else {
@@ -162,6 +175,9 @@ map.on('load', function() {
 
     if (document.getElementById('melinda').checked) {
 
+      $('#legendCandidate').show();
+      $('#legend').hide();
+      
       map.on('mousemove', function candidateFunction(e) {
         var katzEDs = map.queryRenderedFeatures(e.point, {
           layers: ['Katz']
@@ -177,7 +193,7 @@ map.on('load', function() {
   })
 
   // creating a var that selects the radio buttons created in html
-  var radioButton = $("#layerToggle")
+  var radioButton = $('#layerToggle')
 
   // creating function that changes z-index of layer based on radio button selection
   radioButton.on("click", function () {
